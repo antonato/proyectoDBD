6f82b0ae27e3bf24ac988b6c261c5031c83f38d0
Pancho <francisco.nunez.d@usach.cl>
2020-08-03 14:13:24 -0400

    Tablas de Rol, Category y Permissions

---
+++ database/migrations/2020_08_03_181114_create_permissions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->id('idPermission');
+            $table->tinyInteger('create');
+            $table->tinyInteger('read');
+            $table->tinyInteger('update');
+            $table->tinyInteger('delete');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permissions');
+    }
+}
