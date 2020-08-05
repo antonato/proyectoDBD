ca58bf1e19787d4e98795f796dc545a7163b0e0f
Dennis <dennis.urrutia@usach.cl>
2020-08-05 12:12:12 -0400

    se creó las tablas de announcement y  transaction

---
+++ database/migrations/2020_08_05_025423_create_announcements_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAnnouncementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->id();
+            $table->string('NameProduct', 50);
+            $table->float('price', 4, 2);
+            $table->integer('QuialificationAverage');
+            $table->tinyInteger('Disponibility');
+            $table->string('ImageFormat', 4);
+            $table->string('Description', 100);
+            $table->date('PublishedTime',);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('announcements');
+    }
+}
