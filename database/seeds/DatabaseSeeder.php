93d344aaad4835e040aa7d04503806c4d4c0e62a
Pancho <francisco.nunez.d@usach.cl>
2020-08-10 22:06:15 -0400

    Correción de errores: IdRol

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,8 @@ class DatabaseSeeder extends Seeder
         $this->call(AnnouncementTableSeeder::class);
         $this->call(UserTableSeeder::class);
         $this->call(PermissionTableSeeder::class);
-        $this->call(RolPermissionTableSeeder::class);
         $this->call(RolTableSeeder::class);
+        $this->call(RolPermissionTableSeeder::class);
         $this->call(CategoryTableSeeder::class);
         $this->call(ProductCommentTableSeeder::class);
         $this->call(UserAddressTableSeeder::class);
