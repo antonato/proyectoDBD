c6fee806cb1d77049ce6777afd6f71e59510ee2a
Pancho <francisco.nunez.d@usach.cl>
2020-09-09 17:07:48 -0300

    Vista: Corrección Registro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,9 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-dark shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}"> A-riendo </a>
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    <img src="{{ asset('assets\img\logoA-Riendo.png') }}" alt="" width="50px"/>
+                </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
