f507bb4943a56f750fb2fc13af9d08db96977cfa
Pancho <francisco.nunez.d@usach.cl>
2020-09-14 15:25:35 -0300

    Corrección: Archivos eliminados
    A-rreglando

--- resources/views/listAnnouncement.blade.php
+++ resources/views/listAnnouncement.blade.php
@@ -34,32 +34,44 @@
                         <section class="mt-2 mr-2 ml-2 pt-2 pr-2 pl-2 p-0 mb-0 d-flex align-items-center justify-content-center">
                             <strong><h4 class="title-category">CATEGORÍAS</h4></strong>
                         </section>
+                        <a href="{{ action('AnnouncementContr@index') }}" class="item_add">
                         <div class="ind-category d-flex align-items-center">
                             <img class="mr-3 mb-0" src="{{ asset('assets\icons\new.svg') }}" alt="sección lo último" width="33px" height="33px" />
                             <h5>Lo Último</h5>
                         </div>
+                        </a>
+                        <a href="{{ action('AnnouncementContr@category', 'Herramientas') }}" class="item_add">
+                            <div class="ind-category d-flex align-items-center">
+                                <img class="mr-3 mb-0" src="{{ asset('assets\icons\tools.svg') }}" alt="sección herramientas" width="33px" height="33px" />
+                                <h5>Herramientas</h5>
+                            </div>
+                        </a>
+                        <a href="{{ action('AnnouncementContr@category', 'Inmobiliaria') }}" class="item_add">
                         <div class="ind-category d-flex align-items-center">
-                            <img class="mr-3 mb-0" src="{{ asset('assets\icons\tools.svg') }}" alt="sección herramientas" width="33px" height="33px" />
-                            <h5>Herramientas</h5>
-                        </div>
-                        <div class="ind-category d-flex align-items-center">
-                            <img class="mr-3 mb-0" src="{{ asset('assets\icons\build.svg') }}" alt="sección inmobiliaria" width="33px" height="33px" />
+                            <img class="mr-3 mb-0" src="{{ asset('assets\icons\build.svg') }}" alt="sección Inmobiliaria" width="33px" height="33px" />
                             <h5>Inmobiliaria</h5>
                         </div>
+                        </a>       
+                        <a href="{{ action('AnnouncementContr@category', 'Vehiculos') }}" class="item_add">
                         <div class="ind-category d-flex align-items-center">
                             <img class="mr-3 mb-0" src="{{ asset('assets\icons\movil.svg') }}" alt="sección vehículos" width="33px" height="33px" />
                             <h5>Vehículos</h5>
                         </div>
+                        </a>
+                        <a href="{{ action('AnnouncementContr@category', 'Espacios') }}" class="item_add">
                         <div class="ind-category d-flex align-items-center">
                             <img class="mr-3 mb-0" src="{{ asset('assets\icons\party.svg') }}" alt="sección espacios" width="33px" height="33px" />
                             <h5>Espacios</h5>
                         </div>
+                        </a>
+                        <a href="{{ action('AnnouncementContr@category', 'Articulos') }}" class="item_add">
                         <div class="ind-category d-flex align-items-center mb-4 round">
                             <img class="mr-3 mb-0" src="{{ asset('assets\icons\service.svg') }}" alt="sección artículos" width="33px" height="33px" />
                             <h5>Artículos</h5>
                         </div>
+                        </a>
                     </div>
-                </div>
+                </div> 
                 
                 <div>
                     <div id="filter" class="p-2 ml-3 d-flex align-items-center">
@@ -94,7 +106,9 @@
                                     </div>
                                     <div class="d-flex justify-content-center mr-3 col-2">
                                         <div class="button mt-2 justify-content-center w-75">
-                                            <button alt="botón ver">Ver</button>
+                                            <a href="/announcement/{{ $item->id }}">
+                                                <button alt="botón ver">Ver</button>
+                                            </a>
                                         </div> 
                                     </div>
                                 </div>
@@ -112,4 +126,3 @@
     {{-- {!! $announcement ?? ''->render() !!} --}}
 
 </html>
-
